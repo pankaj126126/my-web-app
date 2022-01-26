6713c6d2a155063fa9e33cce63f87a891a89e17b
anurag chhaperwal <anuragchamp71@gmail.com>
2022-01-26 15:13:26 +0530

    server testing
    Signed-off-by: anurag chhaperwal <anuragchamp71@gmail.com>

---
+++ build/precache-manifest.0c8412c340ffd18355f918471e7e29bf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7645d49c06b9ebca376dc5fe154d55d3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "63bbb3ef106789e6e0f9",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "16a30cc2595cb1f391cf",
+    "url": "/static/js/2.5503fb7f.chunk.js"
+  },
+  {
+    "revision": "435881bb28fec4d4e84baf9dfd194218",
+    "url": "/static/js/2.5503fb7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b0881d91f09393d2ba6",
+    "url": "/static/js/3.93a48e44.chunk.js"
+  },
+  {
+    "revision": "63bbb3ef106789e6e0f9",
+    "url": "/static/js/main.74305d8b.chunk.js"
+  },
+  {
+    "revision": "f1900f558e75cb15f1c0",
+    "url": "/static/js/runtime-main.b1ad52bf.js"
+  }
+]);
