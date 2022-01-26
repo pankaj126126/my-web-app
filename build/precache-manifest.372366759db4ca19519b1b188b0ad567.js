acf1157dd8ce5406f35845a96e2d726056ec4a17
anurag chhaperwal <anuragchamp71@gmail.com>
2022-01-26 16:35:19 +0530

    changes
    Signed-off-by: anurag chhaperwal <anuragchamp71@gmail.com>

---
+++ build/precache-manifest.372366759db4ca19519b1b188b0ad567.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "023cc99a44f63122df5af08d22639be5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b410015fa9c0eb8822a",
+    "url": "/static/css/main.1430c76c.chunk.css"
+  },
+  {
+    "revision": "6b358e4416fee4ddc933",
+    "url": "/static/js/2.c4e56f34.chunk.js"
+  },
+  {
+    "revision": "435881bb28fec4d4e84baf9dfd194218",
+    "url": "/static/js/2.c4e56f34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "215fbd197bf7792151c4",
+    "url": "/static/js/3.c1a14020.chunk.js"
+  },
+  {
+    "revision": "4b410015fa9c0eb8822a",
+    "url": "/static/js/main.2aac3949.chunk.js"
+  },
+  {
+    "revision": "e874539dc650a2911d72",
+    "url": "/static/js/runtime-main.da853643.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0cffaf313703dfa57a30bb8493e9a842.js"
+  "/precache-manifest.372366759db4ca19519b1b188b0ad567.js"
 );
 
 self.addEventListener('message', (event) => {
